b5ca86db044e2ca0c884da79e7c94cd29289944a
LimaoC <80520563+LimaoC@users.noreply.github.com>
2021-06-30 16:53:32 +1000

    Update server.py
    - Changed some class methods to regular functions
    - Added functionality to fetch & use server icon for Discord message embeds

--- src/server.py
+++ src/server.py
@@ -5,6 +5,7 @@ server used by the bot
 import os
 import requests
 import json
+import urllib.request
 from sqlitedict import SqliteDict
 
 # Accesses bot_settings database
@@ -40,39 +41,56 @@ class Server:
         bot_settings["server_ip"] = new_ip
         self._ip = new_ip
 
-    def get_online_players(self, ip) -> str:
-        """
-        Returns a message indicating the number of players that are online, and
-        a list (up to 10 max) of players that are online.
-        """
-        MAX_PLAYERS = 10
 
-        # Get json data from API for given server ip
-        if ip is None:  # use default server ip
-            ip = self._ip
+def get_server_icon(ip) -> str:
+    """
+    Returns the path to the server icon (if any).
+    """
+    IMAGE_PATH = "./src/img/servericon.jpg"
 
-        response = requests.get(f"https://api.mcsrvstat.us/2/{ip}")
-        json_data = json.loads(response.text)
+    response = requests.get(f"https://api.mcsrvstat.us/2/{ip}")
+    json_data = json.loads(response.text)
 
-        if json_data["online"]:  # server is online
-            num_players = json_data["players"]["online"]
-            message = (f"There are {num_players} player(s) currently online "
-                       f"on {ip}.")
+    try:
+        icon = json_data["icon"]
 
-            if num_players > MAX_PLAYERS:  # list player names if there are > 0
-                try:  # API doesn't contain the player list if it is too large
-                    for player in json_data["players"]["list"][:MAX_PLAYERS]:
-                        message += "\n- " + player
-                    message += f"\n... and {num_players - MAX_PLAYERS} more"
-                except KeyError:
-                    pass
-            elif num_players > 0:
-                for player in json_data["players"]["list"]:
+        # Convert data URI from API to image
+        with open(IMAGE_PATH, "wb") as image:
+            image.write(urllib.request.urlopen(icon).file.read())
+        return IMAGE_PATH
+    except KeyError:  # server does not have an icon
+        return "./src/img/defaultservericon.png"
+
+
+def get_online_players(ip) -> str:
+    """
+    Returns a message indicating the number of players that are online, and
+    a list (up to 10 max) of players that are online.
+    """
+    MAX_PLAYERS = 10
+
+    response = requests.get(f"https://api.mcsrvstat.us/2/{ip}")
+    json_data = json.loads(response.text)
+
+    if json_data["online"]:  # server is online
+        num_players = json_data["players"]["online"]
+        message = (f"There are {num_players} player(s) currently online "
+                   f"on {ip}.")
+
+        if num_players > MAX_PLAYERS:  # list player names if there are > 0
+            try:  # API doesn't contain the player list if it is too large
+                for player in json_data["players"]["list"][:MAX_PLAYERS]:
                     message += "\n- " + player
-        else:
-            message = f"{ip} is currently offline."
+                message += f"\n... and {num_players - MAX_PLAYERS} more"
+            except KeyError:
+                pass
+        elif num_players > 0:
+            for player in json_data["players"]["list"]:
+                message += "\n- " + player
+    else:
+        message = f"{ip} is currently offline."
 
-        return message
+    return message
 
 
 if __name__ == "__main__":
