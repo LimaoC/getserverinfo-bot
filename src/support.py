3a5bb1afaed55df6962a5ce700d27949c3377cc6
LimaoC <80520563+LimaoC@users.noreply.github.com>
2021-07-01 11:31:05 +1000

    Update support.py

--- src/support.py
+++ src/support.py
@@ -1,3 +1,8 @@
+# Bot embeds
 SUCCESS_MESSAGE = "Success!"
 FAIL_MESSAGE = "Error"
 BOT_NAME = "GetServerInfo"
+
+# Server icon location
+ICON_PATH = "./src/img/servericon.jpg"
+DEFAULT_ICON_PATH = "./src/img/defaultservericon.png"
